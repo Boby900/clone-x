3628dbc74e15feec94eb5de3c00dae29b06b7a7b
Boby900 <bobytiwari310@gmail.com>
2024-07-13 18:04:34 +0530

    Update SvelteKit page with new text color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="text-bold  text-center font-bold text-3xl">Welcome to SvelteKit</h1>
-<h2 class="text-2xl text-center">this thing is weirdo</h2>
+<h2 class="text-2xl text-teal-200 text-center">this thing is weirdo</h2>
 
