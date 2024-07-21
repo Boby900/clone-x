2e7dab6453c7c732fea67b4d20c398215deb2860
Boby900 <bobytiwari310@gmail.com>
2024-07-21 08:25:26 +0530

    Revert "chore: Update npm dependencies and add new packages"
    This reverts commit 9eaf70878fa0b15237f55285739c18c04ae52289.

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
-import { env } from "../../env/server";
 import * as schema from "./schema";
 config({ path: ".env.local" });
-const sql = neon(env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, {schema});
